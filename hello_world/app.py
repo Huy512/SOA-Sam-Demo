8f195b0e62dc99ea31656955bba3b625ff6d9ce2
Huy512 <huynguyen131997@gmail.com>
2025-07-25 14:50:04 +0700

    Update code to trigger GitHub Actions

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world 2",
+            "message": "hello world 132",
             # "location": ip.text.replace("\n", "")
         }),
     }
